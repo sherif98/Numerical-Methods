d707a1bafba62c083b18a483b67c80c88187358a
tarek <tarek.nawara@gmail.com>
2016-05-03 18:25:47 +0200

    RootSolver finished without any testing :)

--- Numerical/+model/@RootSolver/getAllRoots.m
+++ Numerical/+model/@RootSolver/getAllRoots.m
@@ -7,6 +7,8 @@ allIteration = [];
 precision = [];
 theoError = [];
 allRoots = [];
+numForRound = 10000.0;
+eps = 10.^(-5);
 message = 'error';
 % Summary: find all the roots in the given region
 % spliting the total range into equally spaced ranges
@@ -16,16 +18,24 @@ for i=1:(numel(Arr) - 1)
    % try to find a root in this region
    xLower = Arr(i);
    xUpper = Arr(i+1);
+   flag = 0;
    for j=(i+1):(numel(Arr) - 1)
        xUpper = Arr(j);
        if (obj.equation(xUpper) * obj.equation(xLower) < 0)
+           flag = 1;
            break;
        end
    end
-       if (obj.equation(xLower) == 0)
-           allRoots = [allRoots,xLower];
-       elseif (obj.equation(xUpper) == 0)
-               allRoots = [allRoots,xUpper];
+       if (flag == 0)
+           continue;
+       elseif (abs(obj.equation(xLower)) <= eps)
+           xLower = round(xLower * numForRound) / numForRound;
+           allRoots = [allRoots xLower];
+           
+       elseif (abs(obj.equation(xUpper)) <= eps)
+           xUpper = round(xUpper * numForRound) / numForRound;
+           allRoots = [allRoots xUpper];
+               
        elseif (obj.equation(xLower) * obj.equation(xUpper) < 0)
            [xNumOfIterations,xExecutionTime,xAllIteration,xAproxRoot,xPrecision,xTheoError,xMessage] = obj.Bisection(xLower,xUpper);
            % if Succeeded to find the root then append it to the answer
@@ -35,21 +45,23 @@ for i=1:(numel(Arr) - 1)
                allIteration = [allIteration;xAllIteration];
                precision = [precision;xPrecision];
                theoError = [theoError;xTheoError];
+               xAproxRoot = round(xAproxRoot * numForRound) / numForRound;
                allRoots = [allRoots xAproxRoot];
            
-%            else
-%                [xNumOfIterations,xExecutionTime,xAllIteration,xAproxRoot,xPrecision,xTheoError,xMessage] = obj.FalsePosition(xLower,xUpper);
-%                if (strcmp(xMessage,'Success'))
-%                    message = 'Success';
-%                    numOfIterations = [numOfIterations;xNumOfIterations];
-%                    allIteration = [allIteration;xAllIteration];
-%                    precision = [precision;xPrecision];
-%                    theoError = [theoError;xTheoError];
-%                    allRoots = [allRoots;xAproxRoot];
-%                end
+           else
+               [xNumOfIterations,xExecutionTime,xAllIteration,xAproxRoot,xPrecision,xTheoError,xMessage] = obj.FalsePosition(xLower,xUpper);
+               if (strcmp(xMessage,'Success'))
+                   message = 'Success';
+                   numOfIterations = [numOfIterations;xNumOfIterations];
+                   allIteration = [allIteration;xAllIteration];
+                   precision = [precision;xPrecision];
+                   theoError = [theoError;xTheoError];
+                   xAproxRoot = round(xAproxRoot * numForRound) / numForRound;
+                   allRoots = [allRoots xAproxRoot];
+               end
            end 
        end
 end
 executionTime = toc;
-allRoots = unique(allRoots, 'sorted','rows','first');
+allRoots = unique(allRoots);
 end
