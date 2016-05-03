5222e5500f2f944b7ecda7f267c1599e1a814c91
tarek <tarek.nawara@gmail.com>
2016-05-03 03:03:47 +0200

    almost there :)

--- Numerical/+model/@RootSolver/Newton.m
+++ Numerical/+model/@RootSolver/Newton.m
@@ -1,3 +1,31 @@
-function [numOfIterations,executionTime,allIteration,aproxRoot,precision,theoError] = Newton(obj,initialGuess)
+function [numOfIterations,executionTime,allIteration,aproxRoot,precision,theoError,message] = Newton(obj,initialGuess)
 % implementation goes here
+tic
+syms x
+df = diff(obj.equation, x);
+var = obj.equation(initialGuess);
+previous = var;
+numOfIterations = 0;
+allIteration = [];
+precision = -1;
+executionTime = 0;
+message = 'Success';
+theoError = 0;
+for i=1:1:obj.maxNumOfIterations
+    numOfIterations = numOfIterations + 1;
+    x = previous;
+    var = previous - ((obj.equation(previous) / subs(df)));
+    precision = (abs(var - previous)) / var;
+    allIteration = [allIteration;previous,var,precision];
+    if (precision <= obj.precision)
+        aproxRoot = var;
+        return;
+    end
+    previous = var;
+    aproxRoot = var;
+end
+if (numOfIterations >= obj.maxNumOfIterations)
+    message = 'no root found at this precision';
+end
+executionTime = toc;
 end
