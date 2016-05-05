74f4a88b5a63fa437bd58d8089ca6711d60c1647
Ramadan <Ramadan@RamadanPC>
2016-05-05 19:36:27 +0200

    Lagrang

--- Numerical/+model/@Interpolation/Interpolation.m
+++ Numerical/+model/@Interpolation/Interpolation.m
@@ -12,12 +12,12 @@ classdef Interpolation
             obj.points = points;
         end
         % @param queries: array of the points on x axis
-        % @return polynomail: array of the coeffcients sorted in descending
-        % order
+        % @return polynomail: The output equation
         % @return solution: array for each query in the same order
         % @return exeTime: the time of performing the calculation
+        % @return message: an string equal to sucess or error massage
         [exeTime,polynomial,solution] = Newton(obj, queries);
-        [exeTime,polynomial,solution] = Lagrang(obj, queries);
+        [exeTime,polynomial,solution,message] = Lagrang(obj, queries);
     end 
 end
 
--- Numerical/+model/@Interpolation/Lagrang.m
+++ Numerical/+model/@Interpolation/Lagrang.m
@@ -1,3 +1,32 @@
-function [exeTime,polynomial,solution] = Lagrang(obj, queries)
+function [exeTime,polynomial,solution,message] = Lagrang(obj, queries)
 % implementation goes here
+sz = size(obj.points);
+message = '';
+exeTime = tic;
+solution = [];
+syms x;
+polynomial = symfun(0,x);
+if sz(2) ~= 2
+    message = 'not valid points';
+    return;
+end
+sz = sz(1);
+for i = 1 : 1 : sz
+    poly = symfun(1,x);
+    for j = 1 : 1 : sz
+        if i == j
+            continue;
+        end
+        poly = poly * symfun(x-obj.points(j,1),x);
+        poly = poly / symfun(obj.points(i,1)-obj.points(j,1),x);
+    end
+    poly = poly * symfun(obj.points(i,2),x);
+    polynomial = polynomial + poly;
+end
+sz = size(queries);
+for i = 1 : 1 : sz(2)
+    solution = [solution,polynomial(queries(i))];
+end
+message = 'Success';
+exeTime = toc(exeTime);
 end
